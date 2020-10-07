b755a4efb770a951e979c4fb53188f3354952b56
Sanjeev Singh <sanjsin@microsoft.com>
2020-10-07 17:30:09 +0530

    added more tests

--- test/arithmetic.test.ts
+++ test/arithmetic.test.ts
@@ -1,8 +1,13 @@
-import { add } from '../src/arithmetic';
+import { add, sub } from '../src/arithmetic';
 
 describe('test arithemtic suite', () => { 
     test('add method', () => {
         expect(add(2, 4)).toBe(6);
         expect(add(0, 1)).toBe(1);
+    });
+
+    test('sub method', () => {
+        expect(sub(4, 2)).toBe(2);
+        expect(sub(0, 1)).toBe(-1);
     });
 });
